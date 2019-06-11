345ac2aafc6604cab3f4983784f66df54a8e33d1
Disnel Rodriguez <disnelrr@gmail.com>
2019-06-11 08:55:56 -0400

    Set base layout on header

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  *
  * @author Bowriverstudio
  * @link   https://www.getbeans.io
- * @package eans-sample
+ * @package beans-sample
  */
 
 // Include Beans. Do not remove the line below.
@@ -26,6 +26,9 @@ add_action( 'beans_uikit_enqueue_scripts', 'beans_child_enqueue_uikit_assets' );
  *
  * @return void
  */
+
+add_action( 'beans_uikit_enqueue_scripts', 'beans_child_enqueue_uikit_assets' );
+
 function beans_child_enqueue_uikit_assets() {
   // Enqueue uikit overwrite theme folder
 	beans_uikit_enqueue_theme( 'jenkins', get_stylesheet_directory_uri() . '/assets/less/uikit' );
@@ -36,21 +39,6 @@ function beans_child_enqueue_uikit_assets() {
 	// Useful UIKit components
 	beans_uikit_enqueue_components(array( 'sticky', 'slideshow', 'dotnav', 'slidenav' ), 'add-ons');
 	beans_uikit_enqueue_components(array( 'cover', 'section', 'block', 'flex', 'padding', 'grid', 'width', 'animation', 'overlay', 'utility', 'thumbnav', 'thumbnail' ));
-}
-
-// Use style.css instead of style.less
-add_action('wp_enqueue_scripts', 'beans_child_enqueue_styles');
-function beans_child_enqueue_styles()
-{
-    $parent_style = 'parent-style';
-
-    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-    wp_enqueue_style(
-        'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
 }
 
 // Add child theme customizations
@@ -65,14 +53,33 @@ function custom_modify_child_theme()
 
 		//TODO: Remove skip links
 
-		beans_modify_action_callback('beans_header', 'beans_child_modify_header');
+		//beans_add_smart_action('beans_header_after_markup', 'beans_child_modify_header');
+
+		//beans_modify_action_hook('beans_primary_menu', 'site_elements_after_markup');
+
+		beans_add_attribute('beans_site_branding', 'class', 'uk-grid');
+		beans_remove_attribute('beans_primary_menu', 'class', 'uk-float-right');
+
+		beans_modify_action_callback('beans_site_title_tag', 'site_title_description');
+
+		beans_modify_action_hook('beans_primary_menu', 'beans_site_title_tag_after_markup');
 }
 
-function beans_child_modify_header()
+function site_title_description()
 {
 	?>
-
-	<h1>LA PINGA DE POLLO</h1>
-
+		<div>
+			<?php beans_open_markup_e('beans_site_title_tag', 'div', array()  ); ?>
+			<div class="uk-grid">
+				<p class="site-title">
+					<?php echo get_bloginfo( 'name'); ?>
+				</p>
+				<p class="site-description">
+					<?php echo get_bloginfo( 'description'); ?>
+				</p>
+			</div>
+			<?php beans_close_markup_e('beans_site_title_tag', 'div' ); ?>
+		</div>
 	<?php
+
 }
