32b0b43280c96014c6fb4f38fd1548238798778a
Disnel Rodriguez <disnelrr@gmail.com>
2019-06-19 09:56:19 -0400

    Moved utility is_blog_page() function to markup file

--- functions.php
+++ functions.php
@@ -81,24 +81,3 @@ function custom_modify_child_theme()
 		// Blog related customizations
 		beans_remove_action('beans_post_meta');
 }
-
-/**
- * WordPress' missing is_blog_page() function.  Determines if the currently viewed page is
- * one of the blog pages, including the blog home page, archive, category/tag, author, or single
- * post pages.
- *
- * Taken from (https://gist.github.com/wesbos/1189639)
- *
- * @return bool
- */
-function is_blog_page()
-{
-    global $post;
-
-    // Post type must be 'post'.
-    $post_type = get_post_type($post);
-
-    // Check all blog-related conditional tags, as well as the current post type,
-    // to determine if we're viewing a blog page.
-    return ( $post_type === 'post' ) && ( is_home() || is_archive() || is_single() );
-}
